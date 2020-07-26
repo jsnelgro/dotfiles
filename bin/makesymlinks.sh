ee3574d5cfe5b5c2fa1cf8d2d95f57d2b52d3f59
John Snelgrove <johnny.snelgrove@gmail.com>
2020-07-26 10:12:04 -0700

    keep zshrc if reinstalling

--- bin/makesymlinks.sh
+++ bin/makesymlinks.sh
@@ -6,9 +6,9 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc ideavimrc vim zshrc private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
+dir=~/dotfiles                                                           # dotfiles directory
+olddir=~/dotfiles_old                                                    # old dotfiles backup directory
+files="bashrc vimrc ideavimrc vim zshrc private scrotwm.conf Xresources" # list of files/folders to symlink in homedir
 
 ##########
 
@@ -22,45 +22,44 @@ echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
 
-install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+install_zsh() {
+  # Test to see if zshell is installed.  If it is:
+  if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    # install oh-my-zsh if it's not already installed
     if [[ ! -d ~/.oh-my-zsh/ ]]; then
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#        git clone https://github.com/ohmyzsh/ohmyzsh.git
+      sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --keep-zshrc"
     fi
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
+      chsh -s $(which zsh)
     fi
-else
+  else
     # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
+    platform=$(uname)
     # If the platform is Linux, try an apt-get to install zsh and then recurse
     if [[ $platform == 'Linux' ]]; then
-        if [[ -f /etc/redhat-release ]]; then
-            sudo yum install zsh
-            install_zsh
-        fi
-        if [[ -f /etc/debian_version ]]; then
-            sudo apt-get install zsh
-            install_zsh
-        fi
+      if [[ -f /etc/redhat-release ]]; then
+        sudo yum install zsh
+        install_zsh
+      fi
+      if [[ -f /etc/debian_version ]]; then
+        sudo apt-get install zsh
+        install_zsh
+      fi
     # If the platform is OS X, tell the user to install zsh :)
     elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
+      echo "Please install zsh, then re-run this script!"
+      exit
     fi
-fi
+  fi
 }
 
 install_zsh
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+  echo "Moving any existing dotfiles from ~ to $olddir"
+  mv ~/.$file ~/dotfiles_old/
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
 done
